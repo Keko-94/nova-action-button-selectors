6564f025fa4f64fed42006175f6f1db425178df5
Keko-94 <62820283+Keko-94@users.noreply.github.com>
2026-05-04 15:43:20 +0300

    Add extra attributes binding to action buttons

--- resources/js/components/InlineActionDropdown.vue
+++ resources/js/components/InlineActionDropdown.vue
@@ -5,6 +5,7 @@
             <template v-for="action in actions">
                 <button
                     v-if="action.showAsButton"
+                    v-bind="action.extraAttributes || {}"
                     :key="action.uriKey"
                     as="button"
                     :dusk="`${resource.id.value}-inline-action-${action.uriKey}`"
